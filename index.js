64d953bbfadac746c5e1154009ade6f7b5d7dadc
limaleandro1999 <limaleandro1999@gmail.com>
2019-08-08 18:00:12 -0300

    added dump path env variable

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ dotenv.config();
 const dbName = process.env.DB_NAME;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
+const dumpPath = process.env.DUMP_PATH;
 
-console.log(dbName)
-console.log(username)
-console.log(password)
+console.log(dbName);
+console.log(username);
+console.log(password);
+console.log(dumpPath);
 
 console.log('Program has been started');
 
-cron.schedule('0 1 * * *', () => {
+cron.schedule('* * * * *', () => {
     console.log('making dump');
-    shell.exec('mongodump '+ `${dbName ? `--db ${dbName}` : ''} ${username ? `--username ${username}` : ''} ${password ? `--password ${password}` : ''}` +' --gzip --out /home/developer/mongodbbackups/`date +"%Y-%m-%d-%H:%M:%S"`');
+    shell.exec('mongodump '+ `${dbName ? `--db ${dbName}` : ''} ${username ? `--username ${username}` : ''} ${password ? `--password ${password}` : ''}` +' --gzip '+ `--out ${dumpPath ? dumpPath : '/home/'}` + '`date +"%Y-%m-%d-%H:%M:%S"`');
 });
